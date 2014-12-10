118665dd18a1e2789ec15acc29647d9da0c9c8a7
Kruno <krunogr@>
2014-12-10 15:22:35 +0100

    +

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -1,27 +1,33 @@
-$(function (){
-    
-    $("#portfolio img").click(function(){
-        var src= $(this).attr("src");
-        
+$(function () {
+    var current_li;
+    $("#portfolio img").click(function () {
+        var src = $(this).attr("src");
+        current_li = $(this).parent();
         $("#main").attr("src", src);
         $("#frame").fadeIn();
         $("#overlay").fadeIn();
-        
-        
+
+
     });
-    
-    $("#overlay").click(function(){
+
+    $("#overlay").click(function () {
         $(this).fadeOut();
         $("#frame").fadeOut();
-        
+
     });
-    
-    $("#right").click(function (){
-        
-        
+
+    $("#right").click(function () {
+        var next_li = current_li.next();
+        var next_src = next_li.children("img").attr("src");
+        $("#main").attr("src", next_src);
+        current_li = next_li.children("img").parent();
+
     });
-     $("#left").click(function (){
-        
-        
+    $("#left").click(function () {
+        var prev_li = current_li.prev();
+        var prev_src = prev_li.children("img").attr("src");
+        $("#main").attr("src", prev_src);
+        current_li = prev_li.children("img").parent();
+
     })
 });
